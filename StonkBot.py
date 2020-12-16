4e0d2666e9e66cbcc11bc8d97312b83b366fb676
Sam Judd <43186441+samdjudd@users.noreply.github.com>
2020-12-15 21:46:07 -0700

    Update StonkBot.py

--- StonkBot.py
+++ StonkBot.py
@@ -6,22 +6,25 @@ import re
 
 class MyClient(discord.Client):
     async def on_ready(self):
+        # Initializes the user
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
+        # Runs when message is received from Discord server
         if message.author == client.user:
             return
 
         if message.content.startswith('$'):
+            # message starts with '$' followed by numerical value
             to_add = int(re.sub("\$", "", message.content, 1))
             gainz = update_profit(to_add)
             msg = 'Total profits: ${}'.format(gainz)
             await message.channel.send(msg)
             
 def update_profit(to_add, dynamodb=None):
+    # updates the database with new total
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
-
     table = dynamodb.Table('ProfitCounter')
     response = table.get_item(Key={"Name": "Profits"})
     old_number = (response['Item']['Money'])
